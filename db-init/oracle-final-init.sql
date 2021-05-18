67584229daf8bc29e972933c190bb771760e24f1
= <egor@banka.space>
2021-05-18 16:59:11 +0300

    Reformat init script for Oracle final DB

--- db-init/oracle-final-init.sql
+++ db-init/oracle-final-init.sql
@@ -22,11 +22,11 @@ CREATE TABLE person
     name             varchar2(100) NOT NULL,
     surname          varchar2(100) NOT NULL,
     patronymic       varchar2(100),
-    date_of_birth    date   NOT NULL,
+    date_of_birth    date          NOT NULL,
     country_of_birth varchar2(100) NOT NULL,
     city_of_birth    varchar2(100) NOT NULL,
     person_position  varchar2(100),
-    faculty_id       number NOT NULL,
+    faculty_id       number        NOT NULL,
     FOREIGN KEY (faculty_id) REFERENCES faculty (id)
 );
 
@@ -51,8 +51,8 @@ CREATE TABLE study_group
 (
     id               number PRIMARY KEY,
     group_number     varchar2(100) NOT NULL,
-    course_number    number NOT NULL,
-    start_study_date date   NOT NULL,
+    course_number    number        NOT NULL,
+    start_study_date date          NOT NULL,
     end_study_date   date
 );
 
@@ -60,13 +60,13 @@ CREATE TABLE speciality
 (
     id            number PRIMARY KEY,
     name          varchar2(1000) NOT NULL,
-    code          varchar2(100) NOT NULL,
-    qualification varchar2(100) NOT NULL CHECK (
+    code          varchar2(100)  NOT NULL,
+    qualification varchar2(100)  NOT NULL CHECK (
         qualification IN ('бакалавр', 'магистр', 'специалист')
         ),
-    standard_type varchar2(100) NOT NULL CHECK (standard_type IN ('старый', 'новый')),
-    study_form    varchar2(100) NOT NULL CHECK (study_form IN ('очная', 'заочная')),
-    faculty_id    number NOT NULL,
+    standard_type varchar2(100)  NOT NULL CHECK (standard_type IN ('старый', 'новый')),
+    study_form    varchar2(100)  NOT NULL CHECK (study_form IN ('очная', 'заочная')),
+    faculty_id    number         NOT NULL,
     FOREIGN KEY (faculty_id) REFERENCES faculty (id),
     CONSTRAINT check_specialty_code CHECK (
         REGEXP_LIKE(code, '[0-9]{2}\.[0-9]{2}\.[0-9]{2}')
@@ -77,7 +77,7 @@ CREATE TABLE direction
 (
     id   number PRIMARY KEY,
     name varchar2(1000) NOT NULL,
-    code varchar2(100) NOT NULL,
+    code varchar2(100)  NOT NULL,
     CONSTRAINT check_direction_code CHECK (
         REGEXP_LIKE(code, '[0-9]{2}\.[0-9]{2}\.[0-9]{2}')
         )
@@ -88,10 +88,10 @@ CREATE TABLE student
     id             number PRIMARY KEY,
     study_type     varchar2(100) CHECK (study_type IN ('бюджет', 'контракт')),
     is_privileged  number(1, 0) NOT NULL,
-    study_group_id number NOT NULL,
-    direction_id   number NOT NULL,
-    speciality_id  number NOT NULL,
-    person_id      number NOT NULL,
+    study_group_id number       NOT NULL,
+    direction_id   number       NOT NULL,
+    speciality_id  number       NOT NULL,
+    person_id      number       NOT NULL,
     FOREIGN KEY (study_group_id) REFERENCES study_group (id),
     FOREIGN KEY (direction_id) REFERENCES direction (id),
     FOREIGN KEY (speciality_id) REFERENCES speciality (id),
@@ -103,9 +103,9 @@ CREATE TABLE subject
     id               number PRIMARY KEY,
     name             varchar2(100) NOT NULL,
     code             varchar2(100) NOT NULL,
-    lecture_hours    number NOT NULL,
-    workshop_hours   number NOT NULL,
-    laboratory_hours number NOT NULL,
+    lecture_hours    number        NOT NULL,
+    workshop_hours   number        NOT NULL,
+    laboratory_hours number        NOT NULL,
     form_of_control  varchar2(100) NOT NULL
 );
 
@@ -127,7 +127,7 @@ CREATE TABLE conference
     id              number PRIMARY KEY,
     name            varchar2(1000) NOT NULL,
     place           varchar2(1000) NOT NULL,
-    conference_date date NOT NULL
+    conference_date date           NOT NULL
 );
 
 CREATE TABLE publication
@@ -135,27 +135,27 @@ CREATE TABLE publication
     id               number PRIMARY KEY,
     name             varchar2(1000) NOT NULL,
     edition_lang     varchar2(1000) NOT NULL,
-    edition_vol      number NOT NULL,
+    edition_vol      number         NOT NULL,
     edition_place    varchar2(1000) NOT NULL,
     edition_type     varchar2(1000) NOT NULL,
-    citation_index   number NOT NULL,
-    publication_date date   NOT NULL
+    citation_index   number         NOT NULL,
+    publication_date date           NOT NULL
 );
 
 CREATE TABLE project
 (
     id         number PRIMARY KEY,
     name       varchar2(1000) NOT NULL,
-    start_date date NOT NULL,
-    end_date   date NOT NULL
+    start_date date           NOT NULL,
+    end_date   date           NOT NULL
 );
 
 CREATE TABLE library_card
 (
     id           number PRIMARY KEY,
     book_name    varchar2(1000) NOT NULL,
-    receive_date date NOT NULL,
-    return_date  date NOT NULL
+    receive_date date           NOT NULL,
+    return_date  date           NOT NULL
 );
 
 CREATE TABLE project_participant
@@ -189,7 +189,7 @@ CREATE TABLE dorm
 (
     id              number PRIMARY KEY,
     address         varchar2(1000) NOT NULL,
-    number_of_rooms number NOT NULL
+    number_of_rooms number         NOT NULL
 );
 
 CREATE TABLE room_type
@@ -202,9 +202,9 @@ CREATE TABLE room_type
 CREATE TABLE room
 (
     id                        number PRIMARY KEY,
-    dorm_id                   number NOT NULL,
-    room_type_id              number NOT NULL,
-    number_of_occupied_places number NOT NULL,
+    dorm_id                   number       NOT NULL,
+    room_type_id              number       NOT NULL,
+    number_of_occupied_places number       NOT NULL,
     does_have_bugs            number(1, 0) NOT NULL,
     date_of_last_disinfection date,
     FOREIGN KEY (dorm_id) REFERENCES dorm (id),
@@ -247,14 +247,14 @@ CREATE TABLE speciality_subject
 
 CREATE TABLE record_book
 (
-    student_id         number NOT NULL,
-    subject_id         number NOT NULL,
-    teacher_id         number NOT NULL,
-    points             number NOT NULL,
+    student_id         number      NOT NULL,
+    subject_id         number      NOT NULL,
+    teacher_id         number      NOT NULL,
+    points             number      NOT NULL,
     date_of_completion date,
-    mark               number NOT NULL,
+    mark               number      NOT NULL,
     mark_letter        varchar2(1) NOT NULL,
-    exam_date          date   NOT NULL,
+    exam_date          date        NOT NULL,
     FOREIGN KEY (student_id) REFERENCES student (id),
     FOREIGN KEY (subject_id) REFERENCES subject (id),
     FOREIGN KEY (teacher_id) REFERENCES teacher (id),
