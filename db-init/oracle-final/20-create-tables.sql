89cc099d82255c92f5258a71f309419c3ce2d190
= <egor@banka.space>
2021-05-23 15:40:01 +0300

    Do not add constraints to Oracle Final tables

---
+++ db-init/oracle-final/20-create-tables.sql
@@ -0,0 +1,218 @@
+ALTER SESSION SET CURRENT_SCHEMA = hibd;
+
+CREATE TABLE megafaculty
+(
+    id   number PRIMARY KEY,
+    name varchar2(100)
+);
+
+CREATE TABLE faculty
+(
+    id             number PRIMARY KEY,
+    name           varchar2(100),
+    megafaculty_id number
+);
+
+CREATE TABLE person
+(
+    id               number PRIMARY KEY,
+    name             varchar2(100),
+    surname          varchar2(100),
+    patronymic       varchar2(100),
+    date_of_birth    date,
+    country_of_birth varchar2(100),
+    city_of_birth    varchar2(100),
+    person_position  varchar2(100),
+    faculty_id       number
+);
+
+CREATE TABLE person_job
+(
+    id   number PRIMARY KEY,
+    name varchar2(100)
+);
+
+CREATE TABLE teacher
+(
+    id            number PRIMARY KEY,
+    person_id     number,
+    person_job_id number,
+    start_date    date,
+    end_date      date
+);
+
+CREATE TABLE study_group
+(
+    id               number PRIMARY KEY,
+    group_number     varchar2(100),
+    course_number    number,
+    start_study_date date,
+    end_study_date   date
+);
+
+CREATE TABLE speciality
+(
+    id            number PRIMARY KEY,
+    name          varchar2(1000),
+    code          varchar2(1000),
+    qualification varchar2(1000),
+    standard_type varchar2(1000),
+    study_form    varchar2(1000),
+    faculty_id    number
+);
+
+CREATE TABLE direction
+(
+    id   number PRIMARY KEY,
+    name varchar2(1000),
+    code varchar2(1000)
+);
+
+CREATE TABLE student
+(
+    id             number PRIMARY KEY,
+    study_type     varchar2(1000),
+    is_privileged  number(1, 0),
+    study_group_id number,
+    direction_id   number,
+    speciality_id  number,
+    person_id      number
+);
+
+CREATE TABLE subject
+(
+    id               number PRIMARY KEY,
+    name             varchar2(1000),
+    code             varchar2(1000),
+    lecture_hours    number,
+    workshop_hours   number,
+    laboratory_hours number,
+    form_of_control  varchar2(1000)
+);
+
+CREATE TABLE timetable
+(
+    id             number PRIMARY KEY,
+    time_slot      timestamp,
+    room_number    number,
+    teacher_id     number,
+    subject_id     number,
+    study_group_id number
+);
+
+CREATE TABLE conference
+(
+    id              number PRIMARY KEY,
+    name            varchar2(1000),
+    place           varchar2(1000),
+    conference_date date
+);
+
+CREATE TABLE publication
+(
+    id               number PRIMARY KEY,
+    name             varchar2(1000),
+    edition_lang     varchar2(1000),
+    edition_vol      number,
+    edition_place    varchar2(1000),
+    edition_type     varchar2(1000),
+    citation_index   number,
+    publication_date date
+);
+
+CREATE TABLE project
+(
+    id         number PRIMARY KEY,
+    name       varchar2(1000),
+    start_date date,
+    end_date   date
+);
+
+CREATE TABLE library_card
+(
+    id           number PRIMARY KEY,
+    book_name    varchar2(1000),
+    receive_date date,
+    return_date  date
+);
+
+CREATE TABLE project_participant
+(
+    person_id  number,
+    project_id number
+);
+
+CREATE TABLE coauthors
+(
+    person_id      number,
+    publication_id number
+);
+
+CREATE TABLE conference_participant
+(
+    person_id     number,
+    conference_id number
+);
+
+
+CREATE TABLE dorm
+(
+    id              number PRIMARY KEY,
+    address         varchar2(1000),
+    number_of_rooms number
+);
+
+CREATE TABLE room_type
+(
+    id               number PRIMARY KEY,
+    number_of_places number,
+    price            number
+);
+
+CREATE TABLE room
+(
+    id                        number PRIMARY KEY,
+    dorm_id                   number,
+    room_type_id              number,
+    number_of_occupied_places number,
+    does_have_bugs            number(1, 0),
+    date_of_last_disinfection date
+);
+
+CREATE TABLE student_dorm_info
+(
+    id                      number PRIMARY KEY,
+    student_id              number,
+    room_id                 number,
+    date_of_entry           date,
+    date_of_contract_expire date,
+    number_of_warnings      number,
+    paid_till               date,
+    last_check_in           date,
+    last_check_out          date
+);
+
+CREATE TABLE teacher_subject
+(
+    teacher_id number,
+    subject_id number
+);
+
+CREATE TABLE speciality_subject
+(
+    speciality_id number,
+    subject_id    number,
+    semester      number
+);
+
+CREATE TABLE record_book
+(
+    student_id         number,
+    subject_id         number,
+    teacher_id         number,
+    points             number,
+    date_of_completion date,
+    mark               number,
+    mark_letter        varchar2(1),
+    exam_date          date
+);
---
+++ db-init/oracle-final/manual/90-add-constraints.sql
@@ -0,0 +1,194 @@
+
+ALTER TABLE megafaculty
+    MODIFY (name NOT NULL);
+
+
+ALTER TABLE faculty
+    MODIFY (name NOT NULL)
+    ADD FOREIGN KEY (megafaculty_id) REFERENCES megafaculty (id);
+
+ALTER TABLE person
+    MODIFY (name NOT NULL,
+            surname NOT NULL,
+            date_of_birth NOT NULL,
+            country_of_birth NOT NULL,
+            city_of_birth NOT NULL,
+            faculty_id NOT NULL)
+    ADD FOREIGN KEY (faculty_id) REFERENCES faculty (id);
+
+ALTER TABLE person_job
+    MODIFY (name NOT NULL);
+
+ALTER TABLE teacher
+    MODIFY (person_id NOT NULL,
+            person_job_id NOT NULL,
+            start_date NOT NULL,
+            end_date NOT NULL)
+    ADD FOREIGN KEY (person_id) REFERENCES person (id)
+    ADD FOREIGN KEY (person_job_id) REFERENCES person_job (id);
+
+ALTER TABLE study_group
+    MODIFY (group_number NOT NULL,
+            course_number NOT NULL,
+            start_study_date NOT NULL);
+
+ALTER TABLE speciality
+    MODIFY (id PRIMARY KEY,
+            name NOT NULL,
+            code NOT NULL,
+            qualification NOT NULL CHECK (
+                    qualification IN ('бакалавр', 'магистр', 'специалист')),
+            standard_type NOT NULL CHECK (standard_type IN ('старый', 'новый')),
+            study_form NOT NULL CHECK (study_form IN ('очная', 'заочная')),
+            faculty_id NOT NULL)
+    ADD FOREIGN KEY (faculty_id) REFERENCES faculty (id)
+    ADD CONSTRAINT check_speciality_code
+        CHECK (REGEXP_LIKE(code, '[0-9]{2}\.[0-9]{2}\.[0-9]{2}'));
+
+ALTER TABLE direction
+    MODIFY (
+        name NOT NULL,
+        code NOT NULL)
+    ADD CONSTRAINT check_direction_code
+        CHECK (REGEXP_LIKE(code, '[0-9]{2}\.[0-9]{2}\.[0-9]{2}'));
+
+ALTER TABLE student
+    MODIFY (
+        study_type CHECK (study_type IN ('бюджет', 'контракт')),
+        is_privileged NOT NULL,
+        study_group_id NOT NULL,
+        direction_id NOT NULL,
+        speciality_id NOT NULL,
+        person_id NOT NULL)
+    ADD FOREIGN KEY (study_group_id) REFERENCES study_group (id)
+    ADD FOREIGN KEY (direction_id) REFERENCES direction (id)
+    ADD FOREIGN KEY (speciality_id) REFERENCES speciality (id)
+    ADD FOREIGN KEY (person_id) REFERENCES person (id);
+
+ALTER TABLE subject
+    MODIFY (name NOT NULL,
+            code NOT NULL,
+            lecture_hours NOT NULL,
+            workshop_hours NOT NULL,
+            laboratory_hours NOT NULL,
+            form_of_control NOT NULL);
+
+ALTER TABLE timetable
+    MODIFY (
+        time_slot NOT NULL,
+        room_number NOT NULL,
+        teacher_id NOT NULL,
+        subject_id NOT NULL,
+        study_group_id NOT NULL)
+    ADD FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+    ADD FOREIGN KEY (subject_id) REFERENCES subject (id)
+    ADD FOREIGN KEY (study_group_id) REFERENCES study_group (id);
+
+ALTER TABLE conference
+    MODIFY (name NOT NULL,
+            place NOT NULL,
+            conference_date NOT NULL);
+
+ALTER TABLE publication
+    MODIFY (name NOT NULL,
+            edition_lang NOT NULL,
+            edition_vol NOT NULL,
+            edition_place NOT NULL,
+            edition_type NOT NULL,
+            citation_index NOT NULL,
+            publication_date NOT NULL);
+
+ALTER TABLE project
+    MODIFY (
+        name NOT NULL,
+        start_date NOT NULL,
+        end_date NOT NULL);
+
+ALTER TABLE library_card
+    MODIFY (book_name NOT NULL,
+            receive_date NOT NULL,
+            return_date NOT NULL);
+
+ALTER TABLE project_participant
+    MODIFY (
+        person_id NOT NULL,
+        project_id NOT NULL)
+    ADD FOREIGN KEY (person_id) REFERENCES person (id)
+    ADD FOREIGN KEY (project_id) REFERENCES project (id)
+    ADD CONSTRAINT pk_project_participant PRIMARY KEY (person_id, project_id);
+
+ALTER TABLE coauthors
+    MODIFY (
+        person_id NOT NULL,
+        publication_id NOT NULL)
+    ADD FOREIGN KEY (person_id) REFERENCES person (id)
+    ADD FOREIGN KEY (publication_id) REFERENCES publication (id)
+    ADD CONSTRAINT pk_coauthors PRIMARY KEY (person_id, publication_id);
+
+ALTER TABLE conference_participant
+    MODIFY (
+        person_id NOT NULL,
+        conference_id NOT NULL)
+    ADD FOREIGN KEY (person_id) REFERENCES person (id)
+    ADD FOREIGN KEY (conference_id) REFERENCES conference (id)
+    ADD CONSTRAINT pk_conference_participant PRIMARY KEY (person_id, conference_id);
+
+ALTER TABLE dorm
+    MODIFY (address NOT NULL,
+            number_of_rooms NOT NULL);
+
+ALTER TABLE room_type
+    MODIFY (number_of_places NOT NULL,
+            price NOT NULL);
+
+ALTER TABLE room
+    MODIFY (
+        dorm_id NOT NULL,
+        room_type_id NOT NULL,
+        number_of_occupied_places NOT NULL,
+        does_have_bugs NOT NULL)
+    ADD FOREIGN KEY (dorm_id) REFERENCES dorm (id)
+    ADD FOREIGN KEY (room_type_id) REFERENCES room_type (id);
+
+ALTER TABLE student_dorm_info
+    MODIFY (
+        student_id NOT NULL,
+        room_id NOT NULL,
+        date_of_entry NOT NULL,
+        date_of_contract_expire NOT NULL,
+        number_of_warnings NOT NULL,
+        paid_till NOT NULL)
+    ADD FOREIGN KEY (student_id) REFERENCES student (id)
+    ADD FOREIGN KEY (room_id) REFERENCES room (id);
+
+ALTER TABLE teacher_subject
+    MODIFY (
+        teacher_id NOT NULL,
+        subject_id NOT NULL)
+    ADD FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+    ADD FOREIGN KEY (subject_id) REFERENCES subject (id)
+    ADD CONSTRAINT pk_teacher_subject PRIMARY KEY (teacher_id, subject_id);
+
+ALTER TABLE speciality_subject
+    MODIFY (
+        speciality_id NOT NULL,
+        subject_id NOT NULL,
+        semester NOT NULL)
+    ADD FOREIGN KEY (speciality_id) REFERENCES speciality (id)
+    ADD FOREIGN KEY (subject_id) REFERENCES subject (id)
+    ADD CONSTRAINT pk_speciality_subject PRIMARY KEY (speciality_id, subject_id);
+
+ALTER TABLE record_book
+    MODIFY (
+        student_id NOT NULL,
+        subject_id NOT NULL,
+        teacher_id NOT NULL,
+        points NOT NULL,
+        mark NOT NULL,
+        mark_letter NOT NULL,
+        exam_date NOT NULL)
+    ADD FOREIGN KEY (student_id) REFERENCES student (id)
+    ADD FOREIGN KEY (subject_id) REFERENCES subject (id)
+    ADD FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+    ADD CONSTRAINT check_mark CHECK (mark BETWEEN 2 AND 5)
+    ADD CONSTRAINT pk_record_book PRIMARY KEY (student_id, subject_id, teacher_id);
